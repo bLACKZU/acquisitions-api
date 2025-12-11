2331bd5f90ce5b6e8c6500fc6d551a501081bc98
bLACKZU <satyakighosh65@gmail.com>
2025-12-11 22:13:00 +0530

    wip

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 // Setting up express application with right middleware
 
 import express from 'express';
+import logger from './config/logger';
 
 const app = express();
 
 app.get('/', (req, res) => {
+  logger.info('Hello from Acquisitions API');
   res.status(200).send('Hello from API !');
 });
 
